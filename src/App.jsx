15f36fded980b71d3a3f18937f5504c7952f02a4
Philipdeve <thedeveloperphilip@gmail.com>
2023-11-15 21:38:24 +0100

    feat: add product slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Slider from "react-slick";
+import products from "./data";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const settings = {
+    arrows: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 3,
+          initialSlide: 4,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+          initialSlide: 3,
+        },
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+    ],
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <main className="mx-5 my-5 ">
+      <Slider {...settings}>
+        {products.map((product) => (
+          <div
+            key={product.id}
+            className="shadow-md hover:shadow-2xl p-8 grid gap-9 overflow-hidden h-[300px] lg:h-[380px]" >
+            <img
+              src={product.imageUrl}
+              alt={product.name}
+              className="w-full  mb-10"
+            />
+            <h1 className="font-bold text-sm">
+              {product.name.substring(0, 18) + "....."}
+            </h1>
+            <p className="mt-3 text-[#ff6b00]">$ {product.price}</p>
+            <button className="bg-[#ff6b00] text-white text-sm py-2 px-4 rounded-lg my-4 mx-auto ">Add To Cart</button>
+          </div>
+        ))}
+      </Slider>
+    </main>
+  );
+};
 
-export default App
+export default App;
---
+++ src/data.jsx
@@ -0,0 +1,46 @@
+const products = [
+  {
+    id: 1,
+    name: "NIKE Men's Sneaker",
+    imageUrl:
+      "https://m.media-amazon.com/images/I/61U5S6fTOiL._AC_UL480_FMwebp_QL65_.jpg",
+    price: 62,
+  },
+  {
+    id: 2,
+    name: "Nike Court Legacy (Big Kid)",
+    imageUrl:
+      "https://m.media-amazon.com/images/I/41Vin+ognoL._AC_UL480_FMwebp_QL65_.jpg",
+    price: 57,
+  },
+  {
+    id: 3,
+    name: "Product 3NIKE Men's Athletics Shoes",
+    imageUrl:
+      "https://m.media-amazon.com/images/I/519MRhRKGFL._AC_UL480_FMwebp_QL65_.jpg",
+    price: 159.95,
+  },
+  {
+    id: 4,
+    name: "Adidas Women's Cloudfoam Pure 2.0 Running Shoe",
+    imageUrl:
+      "https://m.media-amazon.com/images/I/81d7C4l5GNS._AC_UL480_FMwebp_QL65_.jpg",
+    price: 57,
+  },
+  {
+    id: 5,
+    name: "Adidas Women's Puremotion Adapt 2.0 Shoes Running",
+    imageUrl:
+      "https://m.media-amazon.com/images/I/71Us-E19RxL._AC_UL480_FMwebp_QL65_.jpg",
+    price: 83.66,
+  },
+  {
+    id: 6,
+    name: "Product 3NIKE Men's Athletics Shoes",
+    imageUrl:
+      "https://m.media-amazon.com/images/I/519MRhRKGFL._AC_UL480_FMwebp_QL65_.jpg",
+    price: 159.95,
+  },
+];
+
+export default products;
